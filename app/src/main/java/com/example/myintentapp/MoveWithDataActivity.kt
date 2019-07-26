0538841b2f6d07695c7f85f997750e734d2a1a57
muhammad rahman <rahman9600@gmail.com>
2019-07-26 19:53:25 +0700

    Update MoveWithDataActivity.kt
    changed getStringExtra with getIntExtra

--- app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
+++ app/src/main/java/com/example/myintentapp/MoveWithDataActivity.kt
@@ -13,7 +13,7 @@ class MoveWithDataActivity : AppCompatActivity() {
         val tvDataReceived: TextView = findViewById(R.id.tv_data_received)
 
         val name = intent.getStringExtra(EXTRA_NAME)
-        val age = intent.getStringExtra(EXTRA_AGE)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
 
         val text = "Name : $name, Your Age : $age"
         tvDataReceived.text = text
