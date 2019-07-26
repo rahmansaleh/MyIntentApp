7d3ae7bb4fa9e49ae71c3f4b2e60564e1f920c1f
muhammad rahman <rahman9600@gmail.com>
2019-07-27 06:13:09 +0700

    Update MainActivity.kt
    adding code for button 'DIal Number'

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -20,6 +21,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 22)
                 startActivity(moveIntent)
             }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "085781129984"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
         }
     }
 
@@ -29,8 +36,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
         val btnMoveActivityWithData: Button = findViewById(R.id.btn_move_activity_data)
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
 
         btnMoveActivity.setOnClickListener(this)
         btnMoveActivityWithData.setOnClickListener(this)
+        btnDialPhone.setOnClickListener(this)
     }
 }
